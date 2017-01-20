c314c8e5aefd214c9b57a9bb307b7c5637fa99fa
tumscorpion <prateep2007@gmail.com>
2017-01-20 11:18:05 +0700

    update front search Project

--- backend/views/project/index.php
+++ backend/views/project/index.php
@@ -11,11 +11,13 @@ $baseUrl = \Yii::getAlias('@web');
 
 $this->title = 'โครงการ';
 $this->params['breadcrumbs'][] = $this->title;
-?>
-<div class="project-index">
 
-    <p>
-        <?= Html::a('สร้างโครงการ', ['create'], ['class' => 'btn btn-success','style']) ?>
+include 'extend.php';
+?>
+
+<div class="project-index">
+    <p align="right">
+        <?= Html::a('<i class="fa fa-plus"></i> สร้างโครงการ', ['create'], ['class' => 'btn btn-success','style'=>'text-align: right;']) ?>
     </p>
    	<div class="site-index">
 	<div class="box box-solid">
@@ -26,7 +28,13 @@ $this->params['breadcrumbs'][] = $this->title;
 					<?php echo Html::textInput('name', $name, ['id'=> 'project_name', 'class'=> 'form-control', 'placeholder'=> 'ชื่อโครงการ', 'onchange'=>'this.form.submit()']);?>
 				</div>
 				<div class="col-md-4">
-					<?php echo Html::textInput('status', $status, ['id'=> 'status', 'class'=> 'form-control', 'placeholder'=> 'สถานะ', 'onchange'=>'this.form.submit()']);?>
+					<select id="status" name="status" class="form-control input" onchange="this.form.submit()">
+						<option value="" <?php if($status == "") echo "selected";?>>--ทั้งหมด--</option>
+                        <option value="1" <?php if($status == 1) echo "selected";?>>เปิด</option>
+						<option value="2" <?php if($status == 2) echo "selected";?>>ปิด</option>
+						<option value="3" <?php if($status == 3) echo "selected";?>>ยกเลิก</option>
+						<option value="4" <?php if($status == 4) echo "selected";?>>ถูกลบ</option>
+					</select>
 				</div>
 				<div class="col-md-4">
 					<?php echo Html::textInput('sort', $sort, ['id'=> 'sort', 'class'=> 'form-control', 'placeholder'=> 'จัดเรียง', 'onchange'=>'this.form.submit()']);?>
@@ -44,9 +52,22 @@ $this->params['breadcrumbs'][] = $this->title;
 			<div class="col-md-4">
 				<div class="box box-solid">
 					<div class="box-header with-border box-height">
-						<span>
+						<div class="text-right">
+							<?php echo $lebel[$field->status]; ?>
+						</div>
+						<div class="text-left">
 							<?php echo $field->project_name; ?>
-						</span>
+						</div>
+					</div>
+					<div class="box-body-height">
+						<div class="">
+							<div class="text-right">
+								<a class="btn btn-circle btn-icon-only" href="#">
+									<span>รายละเอียด</span>
+									<i class="fa fa-angle-right"></i>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -55,4 +76,5 @@ $this->params['breadcrumbs'][] = $this->title;
 		<?php } ?>
 	<?php endforeach; ?>
 	</div>
+	
 </div>
