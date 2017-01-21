f483088bc745c1a8764d3eba2a60bb8ab161b4cc
Msi <Msi@DESKTOP-90LO63V>
2017-01-21 19:10:54 +0700

    task

--- backend/views/task/index.php
+++ backend/views/task/index.php
@@ -11,27 +11,99 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="task-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    
 
     <p>
         <?= Html::a('Create Task', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            '_id',
-            'task_name',
-            'start_date',
-            'end_date',
-            'status',
-            // 'create_date',
-            // 'create_by',
-            // 'member',
-            // 'description',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+    <div class="col-md-12">
+    
+  	 <div class="col-md-4">
+  	
+    <div class="box-footer text-black">
+               TO DO
+                
+                  <!-- Progress bars -->
+                  <div>
+                  <div class="clearfix">
+                    <span class="pull-left">Task #1</span>
+                    <small class="pull-right">90%</small>
+                  </div>
+                  <div class="progress xs">
+                    <div class="progress-bar progress-bar-green" style="width: 90%;"></div>
+                  </div>
+			</div>
+			<div>
+                  <div class="clearfix">
+                    <span class="pull-left">Task #2</span>
+                    <small class="pull-right">70%</small>
+                  </div>
+                  <div class="progress xs">
+                    <div class="progress-bar progress-bar-green" style="width: 70%;"></div>
+                  </div>
+               </div>
+               
+             
+              <!-- /.row -->
+            </div>
+            </div>
+             <div class="col-md-4">
+  	
+    <div class="box-footer">
+    			DOING
+                  <!-- Progress bars -->
+                  <div>
+                  <div class="clearfix">
+                    <span class="pull-left">Task #1</span>
+                    <small class="pull-right">90%</small>
+                  </div>
+                  <div class="progress xs">
+                    <div class="progress-bar progress-bar-green" style="width: 90%;"></div>
+                  </div>
+			</div>
+			<div>
+                  <div class="clearfix">
+                    <span class="pull-left">Task #2</span>
+                    <small class="pull-right">70%</small>
+                  </div>
+                  <div class="progress xs">
+                    <div class="progress-bar progress-bar-green" style="width: 70%;"></div>
+                  </div>
+               </div>
+               
+             
+              <!-- /.row -->
+            </div>
+            </div>
+             <div class="col-md-4">
+  	
+    <div class="box-footer text-black">
+               DONE
+                
+                  <!-- Progress bars -->
+                  <div>
+                  <div class="clearfix">
+                    <span class="pull-left">Task #1</span>
+                    <small class="pull-right">90%</small>
+                  </div>
+                  <div class="progress xs">
+                    <div class="progress-bar progress-bar-green" style="width: 90%;"></div>
+                  </div>
+			</div>
+			<div>
+                  <div class="clearfix">
+                    <span class="pull-left">Task #2</span>
+                    <small class="pull-right">70%</small>
+                  </div>
+                  <div class="progress xs">
+                    <div class="progress-bar progress-bar-green" style="width: 70%;"></div>
+                  </div>
+               </div>
+               
+             
+              <!-- /.row -->
+            </div>
+            </div>
+            </div>
+    
 </div>
